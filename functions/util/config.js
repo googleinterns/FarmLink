d47c3a69400f18feca81dd0e06cf2656b8f0302d
Andrew Hojel <alhojelmx@gmail.com>
2020-07-21 11:29:51 -0600

    initial backend setup

---
+++ functions/util/config.js
@@ -0,0 +1,12 @@
+//config.js
+
+module.exports = {
+    apiKey: "AIzaSyDSPAadPOPcdIXimF1DTzKcHcp7TKa0ht8",
+    authDomain: "farmlink-48502.firebaseapp.com",
+    databaseURL: "https://farmlink-48502.firebaseio.com",
+    projectId: "farmlink-48502",
+    storageBucket: "farmlink-48502.appspot.com",
+    messagingSenderId: "784634415906",
+    appId: "1:784634415906:web:da7c908cf660024bb8b37b",
+    measurementId: "G-GC72GD0Q42"
+}
