8c6ef43841ad0665abeab40c2aef5e6dc1254bbe
Andrew Hojel <alhojelmx@gmail.com>
2020-07-21 15:07:04 -0600

    adding the login, signup, and home page

---
+++ view/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import login from './pages/login';
+import signup from './pages/signup';
+import home from './pages/home';
+
+import React from 'react';
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+            <Route exact path="/login" component={login}/>
+            <Route exact path="/signup" component={signup}/>
+            <Route exact path="/" component={home}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+export default App;
