719013e35d305c4de631c2f700fc0a774974a33b
angelinechen9 <angelinechen9@gmail.com>
2020-07-28 10:03:42 -0700

    implement RESTful API for farms

---
+++ functions/APIs/farms.js
@@ -0,0 +1,97 @@
+const { db } = require('../util/admin');
+
+exports.getAllFarms = (request, response) => {
+    db.collection('farms').get()
+    .then((data) => {
+        let farms = [];
+        data.forEach((doc) => {
+            farms.push({
+                farmId: doc.id,
+                farmName: doc.data().farmName,
+                location: doc.data().location,
+                hours: doc.data().hours,
+                transportation: doc.data().transportation,
+                contactName: doc.data().contactName,
+                contactPhone: doc.data().contactPhone,
+                contactEmail: doc.data().contactEmail,
+                loadingDock: doc.data().loadingDock,
+                forklift: doc.data().forklift,
+                farmTags: doc.data().farmTags,
+            });
+        });
+        return response.json(farms);
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+exports.getOneFarm = (request, response) => {
+    db.doc(`/farms/${request.params.farmId}`).get()
+    .then((doc) => {
+        return response.json(doc.data());
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+exports.postOneFarm = (request, response) => {
+    const newFarmItem = {
+        farmName: request.body.farmName,
+        location: request.body.location,
+        hours: request.body.hours,
+        transportation: request.body.transportation === 'true',
+        contactName: request.body.contactName,
+        contactPhone: request.body.contactPhone,
+        contactEmail: request.body.contactEmail,
+        loadingDock: request.body.loadingDock === 'true',
+        forklift: request.body.forklift === 'true',
+        farmTags: request.body.farmTags,
+    };
+    db
+        .collection('farms')
+        .add(newFarmItem)
+        .then((doc) => {
+            let responseFarmItem = newFarmItem;
+            responseFarmItem.id = doc.id;
+            return response.json(responseFarmItem);
+        })
+        .catch((err) => {
+            console.log(err);
+            return response.status(500).json({error: err.code});
+        });
+}
+
+exports.deleteFarm = (request, response) => {
+    const document = db.doc(`/farms/${request.params.farmId}`);
+    document
+        .get()
+        .then((doc) => {
+            if (!doc.exists) {
+                return response.status(404).json({ error: 'Farm Object not found' })
+            }
+            return document.delete();
+        })
+        .then(() => {
+            response.json({ message: 'Delete successfull' });
+        })
+        .catch((err) => {
+            console.log(err);
+            return response.status(500).json({error: err.code});
+        });
+}
+
+exports.editFarm = (request, response) => {
+    let document = db.collection('farms').doc(`${request.params.farmId}`)
+    document.update(request.body)
+    .then(() => {
+        response.json({message: 'Updated successfully'});
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,13 @@ const {
     deleteProduce,
     editProduce,
 } = require('./APIs/produce')
+const {
+    getAllFarms,
+    postOneFarm,
+    getOneFarm,
+    deleteFarm,
+    editFarm,
+} = require('./APIs/farms')
 
 exports.api = functions.https.onRequest(app);
 app.get('/produce', auth, getAllProduce);
@@ -18,6 +25,11 @@ app.get('/produce/:produceId', auth, getOneProduce);
 app.post('/produce', auth, postOneProduce);
 app.delete('/produce/:produceId', auth, deleteProduce);
 app.put('/produce/:produceId', auth, editProduce);
+app.get('/farms', auth, getAllFarms);
+app.get('/farms/:farmId', auth, getOneFarm);
+app.post('/farms', auth, postOneFarm);
+app.delete('/farms/:farmId', auth, deleteFarm);
+app.put('/farms/:farmId', auth, editFarm);
 
 const {
     loginUser,
