51144a2af635e223bdd023a3e3c998d049c29416
Andrew Hojel <alhojelmx@gmail.com>
2020-07-29 17:32:51 -0600

    generated react base application

--- view/src/App.js
+++ view/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import login from './pages/login';
-import signup from './pages/signup';
-import home from './pages/home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
+import signup from "./pages/signup";
+import home from "./pages/home";
 
-import React from 'react';
+import React from "react";
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-            <Route exact path="/login" component={login}/>
-            <Route exact path="/signup" component={signup}/>
-            <Route exact path="/" component={home}/>
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/" component={home} />
         </Switch>
       </div>
     </Router>
--- view/src/util/auth.js
+++
@@ -1,6 +0,0 @@
-export const authMiddleWare = (history) => {
-    const authToken = localStorage.getItem('AuthToken');
-    if(authToken === null){
-        history.push('/login')
-    }
-}
