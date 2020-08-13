741a5ff0ed4c244584e5975426d9278448e99f5d
angelinechen9 <angelinechen9@gmail.com>
2020-08-13 10:50:58 -0700

    comment code

---
+++ functions/APIs/foodbanks.js
@@ -0,0 +1,132 @@
+const { db } = require('../util/admin');
+
+/*
+display a list of all food banks
+GET /foodbanks
+success response: array of food bank objects
+*/
+exports.getAllFoodBanks = (request, response) => {
+    db.collection('foodbanks').get()
+    .then((data) => {
+        let foodbanks = [];
+        data.forEach((doc) => {
+            foodbanks.push({
+                foodbankId: doc.id,
+                foodbankName: doc.data().foodbankName,
+                location: doc.data().location,
+                hours: doc.data().hours,
+                contacts: doc.data().contacts,
+                forklift: doc.data().forklift,
+                pallet: doc.data().pallet,
+                loadingDock: doc.data().loadingDock,
+                maxLoadSize: doc.data().maxLoadSize,
+                refrigerationSpaceAvailable: doc.data().refrigerationSpaceAvailable,
+                foodbankTags: doc.data().foodbankTags,
+            });
+        });
+        return response.json(foodbanks);
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+display a specific food bank
+GET /foodbanks/:id
+success response: food bank object
+*/
+exports.getOneFoodBank = (request, response) => {
+    db.doc(`/foodbanks/${request.params.foodbankId}`).get()
+    .then((doc) => {
+        return response.json(doc.data());
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+create a new food bank
+POST /foodbanks
+data params:
+{
+    foodbankName: [string],
+    location: [string],
+    hours: [array],
+    contacts: [array],
+    forklift: [boolean],
+    pallet: [boolean],
+    loadingDock: [boolean],
+    maxLoadSize: [number],
+    refrigerationSpaceAvailable: [number],
+    foodbankTags: [array]
+}
+success response: food bank object
+*/
+exports.postOneFoodBank = (request, response) => {
+    const newFoodBankItem = {
+        foodbankName: request.body.foodbankName,
+        location: request.body.location,
+        hours: request.body.hours,
+        contacts: request.body.contacts,
+        forklift: request.body.forklift === 'true',
+        pallet: request.body.pallet === 'true',
+        loadingDock: request.body.loadingDock === 'true',
+        maxLoadSize: parseFloat(request.body.maxLoadSize),
+        refrigerationSpaceAvailable: parseFloat(request.body.refrigerationSpaceAvailable),
+        foodbankTags: request.body.foodbankTags,
+    };
+    db.collection('foodbanks').add(newFoodBankItem)
+    .then((doc) => {
+        let responseFoodBankItem = newFoodBankItem;
+        responseFoodBankItem.id = doc.id;
+        return response.json(responseFoodBankItem);
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+delete a specific food bank
+DELETE /foodbanks/:id
+success response: {message: 'Delete successfull'}
+*/
+exports.deleteFoodBank = (request, response) => {
+    const document = db.doc(`/foodbanks/${request.params.foodbankId}`);
+    document.get()
+    .then((doc) => {
+        if (!doc.exists) {
+            return response.status(404).json({ error: 'FoodBank Object not found' })
+        }
+        return document.delete();
+    })
+    .then(() => {
+        response.json({ message: 'Delete successfull' });
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+update a specific food bank
+PUT /foodbanks/:id
+success response: {message: 'Updated successfully'}
+*/
+exports.editFoodBank = (request, response) => {
+    let document = db.collection('foodbanks').doc(`${request.params.foodbankId}`)
+    document.update(request.body)
+    .then(() => {
+        response.json({message: 'Updated successfully'});
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
---
+++ functions/APIs/produce.js
@@ -0,0 +1,123 @@
+const { db } = require('../util/admin');
+
+/*
+display a list of all produce
+GET /produce
+success response: array of produce objects
+*/
+exports.getAllProduce = (request, response) => {
+    db.collection('produce').get()
+    .then((data) => {
+        let produce = [];
+        data.forEach((doc) => {
+            produce.push({
+                produceId: doc.id,
+                name: doc.data().name,
+                shippingPresetTemperature: doc.data().shippingPresetTemperature,
+                shippingMaintenanceTemperatureLow: doc.data().shippingMaintenanceTemperatureLow,
+                shippingMaintenanceTemperatureHigh: doc.data().shippingMaintenanceTemperatureHigh,
+                amountMoved: doc.data().amountMoved,
+                price: doc.data().price,
+                pricePaid: doc.data().pricePaid,
+            });
+        });
+        return response.json(produce);
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+display a specific produce
+GET /produce/:id
+success response: produce object
+*/
+exports.getOneProduce = (request, response) => {
+    db.doc(`/produce/${request.params.produceId}`).get()
+    .then((doc) => {
+        return response.json(doc.data());
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+create a new produce
+POST /produce
+data params:
+{
+    name: [string],
+    shippingPresetTemperature: [number],
+    shippingMaintenanceTemperatureLow: [number],
+    shippingMaintenanceTemperatureHigh: [number],
+    amountMoved: [number],
+    price: [number],
+    pricePaid: [number]
+}
+success response: produce object
+*/
+exports.postOneProduce = (request, response) => {
+    const newProduceItem = {
+        name: request.body.name,
+        shippingPresetTemperature: parseFloat(request.body.shippingPresetTemperature),
+        shippingMaintenanceTemperatureLow: parseFloat(request.body.shippingMaintenanceTemperatureLow),
+        shippingMaintenanceTemperatureHigh: parseFloat(request.body.shippingMaintenanceTemperatureHigh),
+        amountMoved: parseFloat(request.body.amountMoved),
+        price: parseFloat(request.body.price),
+        pricePaid: parseFloat(request.body.pricePaid),
+    };
+    db.collection('produce').add(newProduceItem)
+    .then((doc) => {
+        let responseProduceItem = newProduceItem;
+        responseProduceItem.id = doc.id;
+        return response.json(responseProduceItem);
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+delete a specific produce
+DELETE /produce/:id
+success response: {message: 'Delete successfull'}
+*/
+exports.deleteProduce = (request, response) => {
+    const document = db.doc(`/produce/${request.params.produceId}`);
+    document.get()
+    .then((doc) => {
+        if (!doc.exists) {
+            return response.status(404).json({error: 'Produce Object not found'})
+        }
+        return document.delete();
+    })
+    .then(() => {
+        response.json({message: 'Delete successfull'});
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
+
+/*
+update a specific produce
+PUT /produce/:id
+success response: {message: 'Updated successfully'}
+*/
+exports.editProduce = (request, response) => {
+    let document = db.collection('produce').doc(`${request.params.produceId}`)
+    document.update(request.body)
+    .then(() => {
+        response.json({message: 'Updated successfully'});
+    })
+    .catch((err) => {
+        console.log(err);
+        return response.status(500).json({error: err.code});
+    });
+}
