1ab76d38381cbdfb8aa4e4863efe5761331a8b44
fatimazali <alifatima168@gmail.com>
2020-08-18 18:48:17 -0700

    Abstract card rendering into handleResultsRender and bind to this

--- view/src/components/produce.js
+++ view/src/components/produce.js
@@ -147,7 +147,8 @@ class produce extends Component {
 
     this.deleteTodoHandler = this.deleteTodoHandler.bind(this);
     this.handleEditClickOpen = this.handleEditClickOpen.bind(this);
-    this.handleViewOpen = this.handleViewOpen.bind(this);
+	this.handleViewOpen = this.handleViewOpen.bind(this);
+	this.handleResultsRender = this.handleResultsRender.bind(this);
   }
 
   handleChange = (event) => {
@@ -328,6 +329,104 @@ class produce extends Component {
     const handleClose = (event) => {
       this.setState({ open: false });
     };
+
+	// Handles the rendering of filtered produce results into React cards
+	// Return to render jsx? 
+	const handleResultsRender = (results) => {
+		return (
+			<div>
+			{results.map((produce) => (
+			<Grid item xs={12} key={produce.produceId}>
+				<Card className={classes.root} variant="outlined">
+				<CardContent>
+					<Typography variant="h5" component="h2">
+					{produce.name}
+					</Typography>
+					<Box
+					display="flex"
+					flexDirection="row"
+					flexWrap="wrap"
+					p={0}
+					m={0}
+					>
+					<Box p={3}>
+						<Typography
+						className={classes.pos}
+						color="textSecondary"
+						>
+						Shipping Temperatures in Reefer (°F):
+						</Typography>
+						<Typography variant="body2" component="p">
+						Maintenance Temperature:{" "}
+						{produce.shippingMaintenanceTemperatureLow} -{" "}
+						{produce.shippingMaintenanceTemperatureHigh}
+						<br />
+						Preset Temperature:{" "}
+						{produce.shippingPresetTemperature}
+						</Typography>
+					</Box>
+					<Box p={3}>
+						<Typography
+						className={classes.pos}
+						color="textSecondary"
+						>
+						Pricing (in USD / lb):
+						</Typography>
+						<Typography variant="body2" component="p">
+						USDA Price: ${produce.price}
+						<br />
+						Average Price Paid by Farmlink: $
+						{produce.pricePaid}
+						</Typography>
+					</Box>
+					<Box p={3}>
+						<Typography
+						className={classes.pos}
+						color="textSecondary"
+						>
+						Internal Statistics:
+						</Typography>
+						<Typography variant="body2" component="p">
+						Amount Moved by FarmLink (lbs):{" "}
+						{produce.amountMoved}
+						</Typography>
+					</Box>
+					</Box>
+				</CardContent>
+				<CardActions>
+					<Button
+					size="small"
+					color="primary"
+					onClick={() => this.handleViewOpen({ produce })}
+					>
+					{" "}
+					View{" "}
+					</Button>
+					<Button
+					size="small"
+					color="primary"
+					onClick={() =>
+						this.handleEditClickOpen({ produce })
+					}
+					>
+					Edit
+					</Button>
+					<Button
+					size="small"
+					color="primary"
+					onClick={() =>
+						this.deleteTodoHandler({ produce })
+					}
+					>
+					Delete
+					</Button>
+				</CardActions>
+				</Card>
+			</Grid>
+			))}
+		</div>
+		)}
+
 
     if (this.state.uiLoading === true) {
       return (
@@ -552,101 +651,9 @@ class produce extends Component {
 			</Grid>
 			<Grid container spacing={2} alignItem="center">
               <SearchResults
-                value={value}
-                data={data}
-                renderResults={(results) => (
-                  <div>
-                    {results.map((produce) => (
-                      <Grid item xs={12} key={produce.produceId}>
-                        <Card className={classes.root} variant="outlined">
-                          <CardContent>
-                            <Typography variant="h5" component="h2">
-                              {produce.name}
-                            </Typography>
-                            <Box
-                              display="flex"
-                              flexDirection="row"
-                              flexWrap="wrap"
-                              p={0}
-                              m={0}
-                            >
-                              <Box p={3}>
-                                <Typography
-                                  className={classes.pos}
-                                  color="textSecondary"
-                                >
-                                  Shipping Temperatures in Reefer (°F):
-                                </Typography>
-                                <Typography variant="body2" component="p">
-                                  Maintenance Temperature:{" "}
-                                  {produce.shippingMaintenanceTemperatureLow} -{" "}
-                                  {produce.shippingMaintenanceTemperatureHigh}
-                                  <br />
-                                  Preset Temperature:{" "}
-                                  {produce.shippingPresetTemperature}
-                                </Typography>
-                              </Box>
-                              <Box p={3}>
-                                <Typography
-                                  className={classes.pos}
-                                  color="textSecondary"
-                                >
-                                  Pricing (in USD / lb):
-                                </Typography>
-                                <Typography variant="body2" component="p">
-                                  USDA Price: ${produce.price}
-                                  <br />
-                                  Average Price Paid by Farmlink: $
-                                  {produce.pricePaid}
-                                </Typography>
-                              </Box>
-                              <Box p={3}>
-                                <Typography
-                                  className={classes.pos}
-                                  color="textSecondary"
-                                >
-                                  Internal Statistics:
-                                </Typography>
-                                <Typography variant="body2" component="p">
-                                  Amount Moved by FarmLink (lbs):{" "}
-                                  {produce.amountMoved}
-                                </Typography>
-                              </Box>
-                            </Box>
-                          </CardContent>
-                          <CardActions>
-                            <Button
-                              size="small"
-                              color="primary"
-                              onClick={() => this.handleViewOpen({ produce })}
-                            >
-                              {" "}
-                              View{" "}
-                            </Button>
-                            <Button
-                              size="small"
-                              color="primary"
-                              onClick={() =>
-                                this.handleEditClickOpen({ produce })
-                              }
-                            >
-                              Edit
-                            </Button>
-                            <Button
-                              size="small"
-                              color="primary"
-                              onClick={() =>
-                                this.deleteTodoHandler({ produce })
-                              }
-                            >
-                              Delete
-                            </Button>
-                          </CardActions>
-                        </Card>
-                      </Grid>
-                    ))}
-                  </div>
-                )}
+                value={value} // will this render with binding to this? 
+				data={data}
+				renderResults={handleResultsRender}
               />
             </Grid>
           </Container>
